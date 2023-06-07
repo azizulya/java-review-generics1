1a2003ee7534fec1c6f3b8bd8207ce78f0458ffd
Aziza Rakhmonova <arakhmonova@foundationmedicine.com>
2023-06-07 14:07:53 -0400

    functional interface

--- lambda/src/main/java/functionalinterfaces/Examples.java
+++ lambda/src/main/java/functionalinterfaces/Examples.java
@@ -1,6 +1,6 @@
 package functionalinterfaces;
 
-import java.util.function.Predicate;
+import java.util.function.*;
 
 public class Examples {
     public static void main (String[] args) {
@@ -19,8 +19,27 @@ public class Examples {
         System.out.println (result);
 
 
+        //*********CONSUMER*********//
+
+        Consumer<Integer> display = i -> System.out.println (i);
+        display.accept (50);
 
 
+        //*********BICONSUMER*********//
 
+        BiConsumer<Integer, Integer> addTwo  = (x,y) -> System.out.println (x+y);
+        addTwo.accept (100,200);
+
+        //*********FUNCTION*********//
+
+        Function<String, String> fun = s -> "Hello " + s;
+        String str = fun.apply ("Aziza");
+        System.out.println (str);
+
+
+        //*********SUPPLIER*********//
+
+        Supplier<Double> randomValue = () -> Math.random ();
+        System.out.println (randomValue.get ());
     }
 }
