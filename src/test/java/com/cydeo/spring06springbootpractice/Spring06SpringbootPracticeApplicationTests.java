16e6b16055c2ef1882377dab1181bdd24e6270a2
arakhmonova <arakhmonova@d9q4jtvj63.corp.local>
2023-09-25 16:03:13 -0400

    created spring project

---
+++ src/test/java/com/cydeo/spring06springbootpractice/Spring06SpringbootPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring06springbootpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring06SpringbootPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
